53b64a506b3096adbe68d7d706f7d15a7f80d8a8
Thach Nguyen <thachnn80@gmail.com>
2024-09-24 08:03:41 +0700

    Initialize a new branch to bundle `webpack-cli` v3.3

--- webpack.config.js
+++ webpack.config.js
@@ -94,36 +94,91 @@ const commonjs1Patches = [
 
 //
 module.exports = [
-  /* TODO:
-  webpackConfig('import-local', {
-    entry: { index: './node_modules/import-local/index' },
-    output: { libraryTarget: 'commonjs2', filename: '[name]' },
-    externals: { fsevents: 'commonjs2 fsevents' },
+  //
+  // cross-spawn  /^[v=\s]*((4\.([8-9]|\d{2,})|5\.([7-9]|\d{2,}))\b|([6-9]|\d{2,})\.)/  '^4.8.0 || ^5.7.0 || >= 6.0.0'
+  //
+  webpackConfig('/loader-utils', {
+    entry: { 'lib/loader-utils': './node_modules/loader-utils/lib/parseQuery' },
+    output: { libraryTarget: 'commonjs2' },
     module: {
       rules: [
         {
-          test: /\bnode_modules[\\/]import-local\b.index\.js$/i,
+          test: /\bnode_modules[\\/]json5\b.lib\b.*\.js$/i,
           loader: 'webpack/lib/replace-loader',
-          options: { search: /\brequire((\.resolve)?\(\w+)/g, replace: '__non_webpack_require__$1' },
+          // FIXME clean _iterator -> for...of
+          options: { search: /(?<== *)_interopRequire\w+|\b_(?=typeof\()/g, replace: '' },
         },
       ],
     },
-    optimization: {
-      minimizer: [
-        newTerserPlugin({ test: /(\.m?js|[\\/][\w-]+)$/i, terserOptions: { output: { ascii_only: false } } }),
+    optimization: { minimizer: [newTerserPlugin()] },
+  }),
+  webpackConfig('/yargs', {
+    entry: { 'vendor/yargs': './node_modules/yargs/index' },
+    output: { libraryTarget: 'commonjs2' },
+    module: {
+      rules: [
+        {
+          test: /\bnode_modules[\\/](yargs\b.(\w*|lib\b.apply-.*)|(yargs-parser|require-main-.*)\b.index)\.js$/i,
+          loader: 'webpack/lib/replace-loader',
+          options: { search: /(^|[^\w.])require((\.resolve)?\(\w+|\)|;?$)/gm, replace: '$1__non_webpack_require__$2' },
+        },
+        // FIXME comment-out readFile in Y18N._readLocaleFile()
+      ],
+    },
+  }),
+  //
+  webpackConfig('/webpack-cli', {
+    entry: { 'bin/cli': './node_modules/webpack-cli/bin/cli' },
+    externals: {
+      webpack: 'commonjs2 webpack',
+      'loader-utils': 'commonjs2 ../lib/loader-utils',
+      'enhanced-resolve': 'commonjs2 ../lib/enhanced-resolve',
+      micromatch: 'commonjs2 ../vendor/micromatch',
+      yargs: 'commonjs2 ../vendor/yargs',
+    },
+    module: {
+      rules: [
+        {
+          test: /\bnode_modules[\\/](webpack-cli\b.bin.utils.(prompt|convert)-.*|(interpret|import-.*)\b.index)\.js$/i,
+          loader: 'webpack/lib/replace-loader',
+          options: { search: /\brequire((\.resolve)?\(\w+|\.extensions\b)/g, replace: '__non_webpack_require__$1' },
+        },
+        {
+          test: /node_modules[\\/]webpack-cli\b.bin.cli\.js$/i,
+          loader: 'webpack/lib/replace-loader',
+          options: {
+            multiple: [
+              { search: /^#!/, replace: '//$&' }, // unwrap IIFE
+              { search: /^\(function *\(\) *\{[\r\n]([\s\S]*)[\r\n]\}\)\(\);?/m, replace: '$1' },
+              { search: /\brequire(\(['"])(v8-compile-)/, replace: '__non_webpack_require__$1../vendor/$2' },
+            ],
+          },
+        },
+        {
+          test: /node_modules[\\/]webpack-cli\b.package\.json$/i,
+          loader: 'webpack/lib/replace-loader',
+          options: { search: /,\s*"repository":[\s\S]*/, replace: '\n}' },
+        },
       ],
     },
     plugins: [
       newCopyPlugin([
-        { from: '{LICENSE*,!(CONTRIBUTING).md}', context: 'node_modules/import-local' },
+        { from: '{LICENSE*,*.md}', context: 'node_modules/webpack-cli' },
         {
-          from: 'node_modules/import-local/package.json',
-          transform: (s) => String(s).replace(/,\s*"(d(evD)?ependencies|scripts)":\s*\{[^{}]*\}/g, ''),
+          from: 'node_modules/webpack-cli/package.json',
+          transform(content) {
+            const pkg = JSON.parse(String(content).replace(/(,\s*)"scripts"$/m, '$1"lib", "vendor"'));
+
+            pkg.devDependencies = pkg.dependencies;
+            ['dependencies', 'scripts', 'husky', 'lint-staged', 'jest', 'nyc', 'config'].forEach((k) => delete pkg[k]);
+
+            return (pkg.version += '-0'), JSON.stringify(pkg, null, 2) + '\n';
+          },
         },
-        { from: 'node_modules/acorn/dist/bin.js', to: '[name]/acorn', transform: minifyContent },
+        { from: 'node_modules/v8-compile-cache/v8-*.js', to: 'vendor/[name].[ext]', transform: minifyContent },
+        { from: '{lib,vendor}/{enhanced-*,tapable,graceful-*,micromatch,source-*}.*', context: 'node_modules/webpack' },
       ]),
       new BannerPlugin({ banner: '#!/usr/bin/env node', raw: true }),
     ],
   }),
-  */
 ];
