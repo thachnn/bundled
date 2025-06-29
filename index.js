00055c87002f493a30b92963033f0845e70b939b
Thach Nguyen <thachnn80@gmail.com>
2025-06-30 00:32:42 +0700

    Bundle source-code files

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
+/*global navigator*/
 'use strict';
 
-import pico from './posix';
-import * as utils from './lib/utils';
+const pico = require('./posix');
+
+const isWindows = () => {
+  if (typeof navigator !== 'undefined' && navigator.platform) {
+    const platform = navigator.platform.toLowerCase();
+    return platform === 'win32' || platform === 'windows';
+  }
+
+  if (typeof process !== 'undefined' && process.platform) {
+    return process.platform === 'win32';
+  }
+
+  return false;
+};
 
 function picomatch(glob, options, returnState = false) {
   // default to os.platform()
   if (options && options.windows == null) {
     // don't mutate the original options object
-    options = { ...options, windows: utils.isWindows() };
+    options = { ...options, windows: isWindows() };
   }
 
   return pico(glob, options, returnState);
 }
 
 Object.assign(picomatch, pico);
-export default picomatch;
+
+module.exports = picomatch;
